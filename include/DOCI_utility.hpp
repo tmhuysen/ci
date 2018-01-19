0adeaa14628482edebebda832812d533b8d4a327
Tm Huysen <Tobias.Huysentruyt@Ugent.be>
2018-01-19 12:23:07 +0100

    hf to ci

--- include/DOCI_utility.hpp
+++ include/DOCI_utility.hpp
@@ -7,6 +7,8 @@
 
 #include <Eigen/Dense>
 #include <unsupported/Eigen/CXX11/Tensor>
+#include <hf.hpp>
+#include <libwrp.hpp>
 
 struct CI_basis {
     Eigen::MatrixXd one_int; //one electron integral matrix
@@ -16,6 +18,6 @@ struct CI_basis {
     unsigned long n_electrons; //number of electrons
 };
 
-
+CI_basis rhf_to_CI_basis(hf::rhf::RHF& rhf_basis);
 
 #endif //DOCI_HEAD_DOCI_UTILITY_HPP
