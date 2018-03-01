65118846d2560a5879b5d99e3492a12a48eaca00
Laurent Lemmens <laurent.lemmens@ugent.be>
2018-03-01 12:12:13 -0500

    Change more doci->ci.

--- cmake/InstallLibrary.cmake
+++ cmake/InstallLibrary.cmake
@@ -1,7 +1,7 @@
 # In this CMake file, we will provide the necessary commands to make the install target for this library
 
 
-# The target of this project is a library called "doci" (${PROJECT_NAME}).
+# The target of this project is a library called "ci" (${PROJECT_NAME}).
 # To specify that this target should also be exported, we add the EXPORT option. This is used in conjuction with the install(EXPORT) command below
 install(TARGETS ${LIBRARY_NAME}
         EXPORT ${LIBRARY_NAME} ${EXPORT_TYPE}
@@ -17,7 +17,7 @@ install(FILES ${PROJECT_INCLUDE_FILES} DESTINATION ${INCLUDE_INSTALL_DIR})
 
 
 # Export the target library into a ${PROJECT_NAME}Targets.cmake file.
-# The file dociConfig.cmake includes this file, to be able to use this library with a find_package(doci X.Y.Z) call
+# The file ciConfig.cmake includes this file, to be able to use this library with a find_package(ci X.Y.Z) call
 install(EXPORT ${LIBRARY_NAME} DESTINATION ${CMAKE_INSTALL_DIR} FILE ${PROJECT_NAME}Targets.cmake)
 
 
