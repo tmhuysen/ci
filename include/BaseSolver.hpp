9aac7f16978f4a014c4f7a97bb280fea774aae3f
Laurent Lemmens <laurent.lemmens@ugent.be>
2018-03-19 18:12:22 -0400

    Add BaseSolver.

--- include/BaseSolver.hpp
+++ include/BaseSolver.hpp
@@ -2,9 +2,36 @@
 #define CI_BASESOLVER_HPP
 
 
-class BaseSolver {
 
+namespace ci {
+namespace solver {
+
+
+/**
+ *  The abstract and base solver class.
+ */
+class BaseSolver {
+public:
+
+    /*
+     *  DESTRUCTOR
+     */
+    virtual ~BaseSolver() = default;
+
+
+    /*
+     *  PURE VIRTUAL FUNCTIONS
+     */
+    /**
+     *  Solve the eigenvalue problem associated to the type of solver.
+     */
+    virtual void solve() = 0;
 };
+
+
+}  // namespace solver
+}  // namespace ci
+
 
 
 #endif  // CI_BASESOLVER_HPP
