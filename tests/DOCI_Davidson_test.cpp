888e19440fc0f1e4517b94f8352f36319f788659
Laurent Lemmens <laurent.lemmens@ugent.be>
2018-03-25 13:27:55 -0400

    Added a test for H2 6-31G, which passes.

--- tests/DOCI_Davidson_test.cpp
+++ tests/DOCI_Davidson_test.cpp
@@ -10,13 +10,45 @@
 
 
 
-BOOST_AUTO_TEST_CASE ( DOCI_h2_sto3g_Davidson ) {
+BOOST_AUTO_TEST_CASE ( DOCI_h2_sto3g_dense_vs_Davidson ) {
 
     // Prepare an SOBasis from an RHF calculation
     libwint::Molecule h2 ("../tests/reference_data/h2.xyz");
     double internuclear_repulsion_energy = h2.calculateInternuclearRepulsionEnergy();
 
     libwint::AOBasis ao_basis (h2, "STO-3G");
+    ao_basis.calculateIntegrals();
+
+    hf::rhf::RHF rhf (h2, ao_basis, 1.0e-08);
+    rhf.solve();
+
+    Eigen::MatrixXd coefficient_matrix = rhf.get_C_canonical();
+    libwint::SOBasis so_basis (ao_basis, coefficient_matrix);
+
+
+    // Calculate the DOCI energy using full diagonalization of the dense Hamiltonian
+    ci::DOCI doci_dense (so_basis, h2);
+    doci_dense.solve(numopt::eigenproblem::SolverType::DENSE);
+    double doci_energy_dense = doci_dense.get_eigenvalue() + internuclear_repulsion_energy;
+
+
+    // Calculate the DOCI energy using the Davidson algorithm
+    ci::DOCI doci_davidson (so_basis, h2);
+    doci_davidson.solve(numopt::eigenproblem::SolverType::DAVIDSON);
+    double doci_energy_davidson = doci_davidson.get_eigenvalue() + internuclear_repulsion_energy;
+
+
+    BOOST_CHECK(std::abs(doci_energy_dense - doci_energy_davidson) < 1.0e-08);
+}
+
+
+BOOST_AUTO_TEST_CASE ( DOCI_h2_631g_dense_vs_Davidson ) {
+
+    // Prepare an SOBasis from an RHF calculation
+    libwint::Molecule h2 ("../tests/reference_data/h2.xyz");
+    double internuclear_repulsion_energy = h2.calculateInternuclearRepulsionEnergy();
+
+    libwint::AOBasis ao_basis (h2, "6-31G");
     ao_basis.calculateIntegrals();
 
     hf::rhf::RHF rhf (h2, ao_basis, 1.0e-08);
