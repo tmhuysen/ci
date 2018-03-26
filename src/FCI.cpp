9cdb3096b3fced849177630a4fcebadf155eb572
Tm Huysen <Tobias.Huysentruyt@Ugent.be>
2018-03-26 15:39:00 +0200

    FCI implemented

--- src/FCI.cpp
+++ src/FCI.cpp
@@ -18,45 +18,121 @@ namespace ci {
  *  Given a @param matrix_solver, construct the DOCI Hamiltonian matrix in the solver's matrix representation.
  */
 void FCI::constructHamiltonian(numopt::eigenproblem::BaseMatrixSolver* matrix_solver) {
-    /*
+
     // Create the first spin string.
     // TODO: determine when to switch from unsigned to unsigned long, unsigned long long or boost::dynamic_bitset<>
     bmqc::SpinString<unsigned long> spin_string_alpha (0, this->addressing_scheme_alpha);  // spin string with address 0
 
 
-    for (size_t I = 0; I < this->dim; I++) {  // I loops over all the addresses of the alpha spin strings
+    for (size_t Ia = 0; Ia < this->dim_alpha; Ia++) {  // Ib loops over all the addresses of the alpha spin strings
+        bmqc::SpinString<unsigned long> spin_string_beta(0,
+                                                         this->addressing_scheme_alpha);  // spin string with address 0
+        for (size_t Ib = 0; Ib < this->dim_beta; Ib++) {
+            for (size_t p = 0; p < this->K; p++) {  // SO iteration 1
 
-        double diagonal_element = 0.0;
-        for (size_t p = 0; p < this->K; p++) {  // p loops over SOs
-            if (spin_string.annihilate(p)) {  // if p in I
-                diagonal_element += 2 * this->so_basis.get_h_SO(p,p) + this->so_basis.get_g_SO(p,p,p,p);
+                // alpha branch
+                int sign_p = 1;  // sign for the operator acting on the p-th SO
+                if (spin_string_alpha.annihilate(p, sign_p)) {
+                    for (size_t q = 0; q < this->K; q++) {
 
-                for (size_t q = 0; q < p; q++) {  // q loops over SOs
-                    if (spin_string.create(q)) {  // if q not in I
-                        size_t J = spin_string.address(this->addressing_scheme);  // J is the address of a string that couples to I
+                        // alpha one-electron branch
+                        int sign_q = sign_p; // sign for the operator acting on the q-th SO
+                        if (spin_string_alpha.create(q, sign_q)) {
+                            size_t Ja = spin_string_alpha.address(this->addressing_scheme_alpha);
+                            // if alpha is major relative address in the total basis is multiplied by all beta combinations
+                            matrix_solver->addToMatrix(sign_q*this->so_basis.get_h_SO(p, q), Ia * dim_beta + Ib,
+                                                       Ja * dim_beta + Ib);
+                            spin_string_alpha.annihilate(q);  // undo
 
-                        // The loops are p->K and q<p. So, we should normally multiply by a factor 2 (since the summand is symmetric).
-                        // However, we are setting both of the symmetric indices of Hamiltonian, so no factor 2 is required.
-                        matrix_solver->addToMatrix(this->so_basis.get_g_SO(p,q,p,q), I, J);
-                        matrix_solver->addToMatrix(this->so_basis.get_g_SO(p,q,p,q), J, I);
-
-                        spin_string.annihilate(q);  // reset the spin string after the previous creation
+                        }
+                        // alpha-alpha two-electron branch
+                        sign_q = sign_p;
+                        if (spin_string_alpha.annihilate(q, sign_q)) {
+                            for (size_t r = 0; r < this->K; r++) {
+                                int sign_r = sign_q;
+                                if (spin_string_alpha.create(r, sign_r)) {
+                                    for (size_t s = 0; s < this->K; s++) {
+                                        int sign_s = sign_r;
+                                        if (spin_string_alpha.create(s, sign_s)) {
+                                            size_t Ja = spin_string_alpha.address(this->addressing_scheme_alpha);
+                                            matrix_solver->addToMatrix(sign_s*this->so_basis.get_g_SO(p, s, q, r)/2,
+                                                                       Ia * dim_beta + Ib, Ja * dim_beta + Ib);
+                                            spin_string_alpha.annihilate(s);
+                                        }
+                                    }
+                                    spin_string_alpha.annihilate(r);
+                                }
+                            }
+                            spin_string_alpha.create(q);
+                        }
+                        // alpha-beta two-electron branch
+                        sign_q = sign_p;
+                        if (spin_string_beta.annihilate(q, sign_q)) {
+                            for (size_t r = 0; r < this->K; r++) {
+                                int sign_r = sign_q;
+                                if (spin_string_beta.create(r, sign_r)) {
+                                    for (size_t s = 0; s < this->K; s++) {
+                                        int sign_s = sign_r;
+                                        if (spin_string_alpha.create(s, sign_s)) {
+                                            size_t Ja = spin_string_alpha.address(this->addressing_scheme_alpha);
+                                            size_t Jb = spin_string_beta.address(this->addressing_scheme_beta);
+                                            matrix_solver->addToMatrix(sign_s*this->so_basis.get_g_SO(p, s, q, r),
+                                                                       Ia * dim_beta + Ib, Ja * dim_beta + Jb);  // We do not divide by 2 because we will not evaluate this branch in the beta branch.
+                                            spin_string_alpha.annihilate(s);
+                                        }
+                                    }
+                                    spin_string_beta.annihilate(r);
+                                }
+                            }
+                            spin_string_beta.create(q);
+                        }
                     }
+                    spin_string_alpha.create(p);
+                }
+                sign_p = 1;
+                // beta branch 
+                if (spin_string_beta.annihilate(p, sign_p)) {
+                    for (size_t q = 0; q < this->K; q++) {
 
-                    else {  // if q in I
-                        diagonal_element += 2 * (2*this->so_basis.get_g_SO(p,p,q,q) - this->so_basis.get_g_SO(p,q,q,p));
+                        // beta one-electron branch
+                        int sign_q = sign_p; // sign for the operator acting on the q-th SO
+                        if (spin_string_beta.create(q, sign_q)) {
+                            size_t Jb = spin_string_beta.address(this->addressing_scheme_alpha);
+                            // if alpha is major relative address in the total basis is multiplied by all beta combinations
+                            matrix_solver->addToMatrix(sign_q*this->so_basis.get_h_SO(p, q), Ia * dim_beta + Jb,
+                                                       Ia * dim_beta + Jb);
+                            spin_string_beta.annihilate(q);  // undo
+
+                        }
+                        // beta-beta two-electron branch
+                        sign_q = sign_p;
+                        if (spin_string_beta.annihilate(q, sign_q)) {
+                            for (size_t r = 0; r < this->K; r++) {
+                                int sign_r = sign_q;
+                                if (spin_string_beta.create(r, sign_r)) {
+                                    for (size_t s = 0; s < this->K; s++) {
+                                        int sign_s = sign_r;
+                                        if (spin_string_beta.create(s, sign_s)) {
+                                            size_t Jb = spin_string_beta.address(this->addressing_scheme_beta);
+                                            matrix_solver->addToMatrix(sign_s*this->so_basis.get_g_SO(p, s, q, r)/2,
+                                                                       Ia * dim_beta + Ib, Ia * dim_beta + Jb);
+                                            spin_string_beta.annihilate(s);
+                                        }
+                                    }
+                                    spin_string_beta.annihilate(r);
+                                }
+                            }
+                            spin_string_beta.create(q);
+                        }
                     }
-                }  // q
-
-                spin_string.create(p);  // reset the spin string after the previous annihilation
+                    spin_string_beta.create(p);
+                }
             }
-        }  // p
+            spin_string_beta.nextPermutation();
+        }
+        spin_string_alpha.nextPermutation();
 
-        matrix_solver->addToMatrix(diagonal_element, I, I);
-        spin_string.nextPermutation();
-
-    }  // address (I) loop
-    */
+    }
 }
 
 
